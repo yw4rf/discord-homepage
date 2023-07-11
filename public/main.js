8cc11aed36b169ceacde0fe24733faa10a8a584c
Tobias Fioroni <98071440+tobigfioroni@users.noreply.github.com>
2023-07-11 18:13:56 -0300

    Added files, dropdown language menu and added reveal hover animation.

--- public/main.js
+++ public/main.js
@@ -13,3 +13,29 @@ const body = document.querySelector("body");
       navbar.classList.remove("show");
       menuBtn.classList.remove("hide");
     }
+
+    // scroll reveal: 
+
+    function revealSections() {
+      var sections = document.querySelectorAll('.scroll-reveal');
+    
+      sections.forEach(function(section) {
+        if (section.getBoundingClientRect().top <= window.innerHeight * 0.75) {
+          section.style.opacity = 1;
+          section.style.transform = 'translateY(0)';
+        }
+      });
+    }
+    window.addEventListener('scroll', revealSections);
+    window.addEventListener('load', revealSections);
+    
+// DROPDOWN LANGAGE
+
+function toggleDropdown() {
+  var languageList = document.getElementById("language-list");
+  if (languageList.style.display === "none") {
+    languageList.style.display = "block";
+  } else {
+    languageList.style.display = "none";
+  }
+}
